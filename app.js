06721eaa3da934e16b5adb18c2bc6e624f41cbb1
travischantravis <chancheukyin4463@gmail.com>
2020-02-13 16:51:49 -0800

    enable firebase storage

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
+
+// Public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
