dcf5a70b3a9371f9275fb8a825b5e32be37ce195
travischantravis <chancheukyin4463@gmail.com>
2020-02-17 09:59:51 -0800

    fixed cloud

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 const app = express();
 
 app.set("view engine", "ejs");
